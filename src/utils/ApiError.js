af2e5379362ac5d0433a13c43876f5c72b49aaff
vekariaayush04 <136103799+vekariaayush04@users.noreply.github.com>
2023-11-06 23:01:29 +0530

    Update ApiError.js
    MORE TYPOS

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
         statusCode,
         message= "Something went wrong",
         errors = [],
-        statck = ""
+        stack = ""
     ){
         super(message)
         this.statusCode = statusCode
@@ -12,8 +12,8 @@ class ApiError extends Error {
         this.success = false;
         this.errors = errors
 
-        if (statck) {
-            this.stack = statck
+        if (stack) {
+            this.stack = stack
         } else{
             Error.captureStackTrace(this, this.constructor)
         }
