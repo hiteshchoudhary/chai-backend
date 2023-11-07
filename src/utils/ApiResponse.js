7379b3116262f2808c4b26229261cb3222a65702
hiteshchoudhary <hitesh@hiteshchoudhary.com>
2023-11-07 13:42:13 +0530

    minor bug in ApiResponse file

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -2,7 +2,7 @@ class ApiResponse {
     constructor(statusCode, data, message = "Success"){
         this.statusCode = statusCode
         this.data = data
-        this,message = message
+        this.message = message
         this.success = statusCode < 400
     }
 }
