f618ef4551a3d1242027ffc6fc2e7aa5e4d32525
imaman199 <117969240+imaman199@users.noreply.github.com>
2023-11-11 12:33:40 +0530

    some spelling mistake fixed. (lowecase to lowercase)

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -8,7 +8,7 @@ const userSchema = new Schema(
             type: String,
             required: true,
             unique: true,
-            lowecase: true,
+            lowercase: true,
             trim: true, 
             index: true
         },
@@ -16,7 +16,7 @@ const userSchema = new Schema(
             type: String,
             required: true,
             unique: true,
-            lowecase: true,
+            lowercase: true,
             trim: true, 
         },
         fullName: {
