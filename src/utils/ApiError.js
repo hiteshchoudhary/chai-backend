b45f14ff99cc3ec512d11c5428f2a38f61d0ee95
Raj Singh <91987215+Raj-venom@users.noreply.github.com>
2023-11-18 18:00:19 +0545

    fixed stack spelling in ApiError file

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
         statusCode,
         message= "Something went wrong",
         errors = [],
-        statck = ""
+        stack = ""
     ){
         super(message)
         this.statusCode = statusCode
