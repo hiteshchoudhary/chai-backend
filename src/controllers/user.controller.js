591c8f74d4df9970ac2026cca5f328ce8eb10738
hiteshchoudhary <hitesh@hiteshchoudhary.com>
2023-12-13 17:24:57 +0530

    Fix username and email validation in loginUser and process spelling in auth middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -105,10 +105,17 @@ const loginUser = asyncHandler(async (req, res) =>{
     //send cookie
 
     const {email, username, password} = req.body
+    console.log(email);
 
-    if (!username || !email) {
+    if (!username && !email) {
         throw new ApiError(400, "username or email is required")
     }
+    
+    // Here is an alternative of above code based on logic discussed in video:
+    // if (!(username || email)) {
+    //     throw new ApiError(400, "username or email is required")
+        
+    // }
 
     const user = await User.findOne({
         $or: [{username}, {email}]
