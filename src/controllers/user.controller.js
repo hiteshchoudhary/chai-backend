50de39cc9f2ae64e0979a9d49075927b3863de54
Rushikesh0107 <rushikeshdadas03@gmail.com>
2023-12-18 02:26:30 +0530

    fixed if statement logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -185,7 +185,7 @@ const logoutUser = asyncHandler(async(req, res) => {
 const refreshAccessToken = asyncHandler(async (req, res) => {
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-    if (incomingRefreshToken) {
+    if (!incomingRefreshToken) {
         throw new ApiError(401, "unauthorized request")
     }
 
