a675428be30681229a4d3f75752d3e4a24f92cf4
Sagar <04017711721_iot@vvips.edu>
2023-12-18 14:36:15 +0530

    Fix logoutUser function to use null instead of undefined

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -158,6 +158,8 @@ const loginUser = asyncHandler(async (req, res) =>{
 })
 
 const logoutUser = asyncHandler(async(req, res) => {
+
+    // instead of undefined we should use the null
     await User.findByIdAndUpdate(
         req.user._id,
         {
