ea7a40a0124de5b7b8bf67ed21565517506ea3b9
Rashid <siddiqui20042007@gmail.com>
2024-01-08 00:38:15 +0530

    updated tweet and subsciption controllers

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,24 +1,98 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {User} from "../models/user.model.js"
+import mongoose, { isValidObjectId } from "mongoose"
+import { User } from "../models/user.model.js"
 import { Subscription } from "../models/subscription.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-    // TODO: toggle subscription
+
+    try {
+
+        const { channelId } = req.params
+        // TODO: toggle subscription
+
+        const userId = req.user.id;
+
+        // Check if the channel exists
+        const channel = await User.findById(channelId);
+        if (!channel) {
+            throw new ApiError(404, "Channel not found")
+        }
+
+        const subscription = await Subscription.findOne({ subscriber: userId, channel: channelId });
+
+        // user has subcribed to the channel => unsubscribe channel
+        if (subscription) {
+            await subscription.remove();
+            return res.status(200).json(
+                new ApiResponse(200, {}, "Unsubscribed successfully")
+            );
+        }
+        // user has not subcribed to the channel => subscribe channel
+        else {
+            await Subscription.create({
+                subscriber: userId,
+                channel: channelId,
+            });
+
+            return res.status(201).json(
+                new ApiResponse(200, {}, "Subscribed successfully")
+            );
+        }
+    }
+    catch (error) {
+        console.error("Error toggling subscription:", error);
+        return res.status(500).json(
+            new ApiError(500, "Internal Server Error")
+        );
+    }
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+
+    try {
+        const { channelId } = req.params
+
+        // Populate the 'subscriber' field with the 'username' field from the User model
+        const subscribers = await Subscription.find({ channel: channelId })
+            .populate('subscriber', 'username');
+
+        res.status(200).json(
+            new ApiResponse(200, "Subscribers retrieved:", subscribers)
+        );
+
+    } catch (error) {
+        console.error("Error retrieving subscirbers:", error);
+        res.status(500).json(
+            new ApiError(500, "Internal Server Error")
+        );
+    }
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
+
+    try {
+        const { subscriberId } = req.params
+
+        // Populate the 'channel' field with the 'username' field from the User model
+        const subscriptions = await Subscription.find({ subscriber: subscriberId })
+            .populate('channel', 'username');
+
+        res.status(200).json(
+            new ApiResponse(200, "Subscribed channels retrieved:", subscriptions)
+        );
+
+    } catch (error) {
+        console.error("Error retrieving subscribed channels:", error);
+        res.status(500).json(
+            new ApiError(500, "Internal Server Error")
+        );
+    }
+
 })
 
 export {
--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,24 +1,121 @@
 import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { Tweet } from "../models/tweet.model.js"
+import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
-})
+    // TODO: create a new tweet
+    try {
+        const { content, owner } = req.body;
+
+        // Check if the user (owner) exists
+        const user = await User.findById(owner);
+        if (!user) {
+            throw new ApiError(404, "User not found")
+        }
+
+        const newTweet = await Tweet.create({
+            content,
+            owner,
+        });
+
+        res.status(201).json(
+            new ApiResponse(200, "Tweet created:", newTweet)
+        );
+    } catch (error) {
+        console.error("Error creating tweet:", error);
+        res.status(500).json(
+            new ApiResponse(500, "Internal Server Error")
+        );
+    }
+});
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    
+    try {
+        const userId = req.params.userId; 
+
+        // Find tweets by the specified user ID
+        const tweets = await Tweet.find({ owner: userId })
+            .populate('owner', 'username');  
+
+        res.status(200).json(
+            new ApiResponse(200, "Tweets retrieved:", tweets)
+        );
+    } catch (error) {
+        console.error("Error while retrieving tweets:", error);
+        res.status(500).json(
+            new ApiResponse(500, "Internal Server Error")
+        );
+    }
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+
+    try{
+        const {tweetId,updatedContent} = req.body
+
+        const tweet = await Tweet.findById(tweetId)
+
+        
+        // Check if the tweet exists
+        if (!tweet) { 
+            throw new ApiError(404, "Tweet not found")
+        }
+    
+        tweet.content = updatedContent
+        await tweet.save({validateBeforeSave: false})
+    
+        return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Tweet changed successfully"))
+    }
+    catch (error) {
+        console.error("Error updating tweet:", error);
+        return res.status(500).json(
+            new ApiResponse(500, "Internal Server Error")
+        );
+    }
+   
 })
+
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    try {
+        const { tweetId } = req.body;
+        const userId = req.user.id;
+ 
+        const tweet = await Tweet.findById(tweetId);
+
+        // Check if the tweet exists
+        if (!tweet) {
+            throw new ApiError(404, "Tweet not found")
+        }
+
+        // Check if the authenticated user is the owner of the tweet
+        if (tweet.owner.toString() !== userId) {
+            return res.status(403).json(
+                new ApiResponse(403, "Unauthorized: You are not the owner of this tweet")
+            );
+        }
+
+        // Delete the tweet
+        await tweet.remove();
+ 
+        return res.status(200).json(
+            new ApiResponse(200, {}, "Tweet deleted successfully")
+        );
+    } catch (error) {
+        console.error("Error deleting tweet:", error);
+        return res.status(500).json(
+            new ApiResponse(500, "Internal Server Error")
+        );
+    }
 })
 
 export {
