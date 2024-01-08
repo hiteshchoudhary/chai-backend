b4adaa0604d729eb1c86b3fe6a1e84c3ce6a9885
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-01-08 22:29:38 +0530

    Health Check controller added

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,9 +1,20 @@
-import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const healthcheck = asyncHandler(async (req, res) => {
+const healthcheck = asyncHandler(async (_req, res) => {
   //TODO: build a healthcheck response that simply returns the OK status as json with a message
+  const additionalData = {
+    uptime: process.uptime().toFixed(2) + " seconds.",
+  };
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        additionalData,
+        "OK Status, service is working fine."
+      )
+    );
 });
 
 export { healthcheck };
