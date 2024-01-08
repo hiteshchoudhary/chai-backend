9112188714742338ffc00491f69dd278d5ca7269
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-01-08 16:46:25 +0530

    feat: :sparkles: add custom error checks

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,19 +1,36 @@
+import { ApiError } from "./ApiError.js";
+import { MongooseError } from "mongoose";
+import { ApiResponse } from "./ApiResponse.js";
+
 const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
-    }
-}
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => {
+      if (err instanceof ApiError) {
+        return res
+          .status(400)
+          .json(new ApiResponse(err.statusCode, null, err.message));
+      }
 
+      if (err instanceof MongooseError) {
+        return res.status(400).json(new ApiResponse(400, null, err.message));
+      }
 
-export { asyncHandler }
+      if (err instanceof Error) {
+        return res.status(400).json(new ApiResponse(400, null, err.message));
+      }
 
+      return res
+        .status(500)
+        .json(new ApiResponse(500, null, "Internal Server Error"));
+    });
+  };
+};
 
-
+export { asyncHandler };
 
 // const asyncHandler = () => {}
 // const asyncHandler = (func) => () => {}
 // const asyncHandler = (func) => async () => {}
-
 
 // const asyncHandler = (fn) => async (req, res, next) => {
 //     try {
