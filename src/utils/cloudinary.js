5121250f613258ffd4d40bd0e3e1e2537ea8cfab
itzmeimran <ikik790@gmail.com>
2024-01-08 14:22:49 +0530

    added deleteOnCloudinary controller

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,31 +1,53 @@
-import {v2 as cloudinary} from "cloudinary"
-import fs from "fs"
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
-
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
-    try {
-        if (!localFilePath) return null
-        //upload the file on cloudinary
-        const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: "auto"
-        })
-        // file has been uploaded successfull
-        //console.log("file is uploaded on cloudinary ", response.url);
-        fs.unlinkSync(localFilePath)
-        return response;
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfull
+    //console.log("file is uploaded on cloudinary ", response.url);
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
 
-    } catch (error) {
-        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
-        return null;
+const deleteOnCloudinary = async (url) => {
+  try {
+    //Getting public Id
+    const publicId = url.split("/").pop().split(".")[0];
+    // console.log("This is public Id", publicId);
+    //Validating Public ID
+    if (!publicId) {
+      return console.log("No public Id present");
     }
-}
+    const options = {
+      invalidate: true,
+    };
 
+    // Delete the file using the public ID
+    cloudinary.uploader.destroy(publicId, function (error, result) {
+      console.log(result, error);
+    });
+    return console.log("Deleted successfully");
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(500)
+      .json({ message: "something went wrong", data: error.message });
+  }
+};
 
-
-export {uploadOnCloudinary}
+export { uploadOnCloudinary };
