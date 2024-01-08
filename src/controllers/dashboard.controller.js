5ddb3d78a0d20295d16422812d9100933991b18c
itzmeimran <ikik790@gmail.com>
2024-01-08 12:51:11 +0530

    like controller completed

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,20 +1,37 @@
-import mongoose from "mongoose"
-import {Video} from "../models/video.model.js"
-import {Subscription} from "../models/subscription.model.js"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose from "mongoose";
+import { Video } from "../models/video.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-})
+  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+  try {
+  } catch (error) {}
+});
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
-})
-
-export {
-    getChannelStats, 
-    getChannelVideos
+  // TODO: Get all the videos uploaded by the channel
+  try {
+    const user = req.user._id;
+    const channelVideos = await Video.findById({ owner: user });
+    if (!channelVideos) {
+      return new ApiError(404, "videos not found");
     }
+    return res
+      .status(201)
+      .json(
+        new ApiResponse(
+          200,
+          channelVideos,
+          "Fetched Channel Videos Successfully"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(500, "Something went wrong");
+  }
+});
+
+export { getChannelStats, getChannelVideos };
--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,33 +1,96 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
-    //TODO: toggle like on video
-})
+  const { videoId } = req.params;
+  //TODO: toggle like on video
+  try {
+    const userId = req.user._id;
+    const conditions = { likedBy: userId, video: videoId };
+    const like = await Like.find(conditions);
+    if (!like) {
+      const newLike = await Like.create({ video: videoId, likedBy: userId });
+      return res
+        .status(200)
+        .json(new ApiResponse(201, newLike, "Liked successfully"));
+    } else {
+      const removeLike = await Like.findOneAndRemove(conditions);
+      return res
+        .status(200)
+        .json(new ApiResponse(201, removeLike, "Removed like successfully"));
+    }
+  } catch (error) {
+    throw new ApiError(500, "Something went wrong", error.message);
+  }
+});
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
-    //TODO: toggle like on comment
+  const { commentId } = req.params;
+  const userId = req.user._id;
+  //TODO: toggle like on comment
+  try {
+    const conditions = { likedBy: userId, tweet: tweetId };
+    const like = await Like.findOne(conditions);
 
-})
+    if (!like) {
+      const newLike = await Like.create({
+        comment: commentId,
+        likedBy: userId,
+      });
+      return res
+        .status(200)
+        .json(new ApiResponse(201, newLike, "Liked successfully"));
+    } else {
+      const removeLike = await Like.findOneAndRemove(conditions);
+      return res
+        .status(200)
+        .json(new ApiResponse(201, removeLike, "Removed like successfully"));
+    }
+  } catch (error) {
+    throw new ApiError(500, "Something went wrong", error.message);
+  }
+});
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
-    //TODO: toggle like on tweet
-}
-)
+  const { tweetId } = req.params;
+  const userId = req.user._id;
+  //TODO: toggle like on tweet
+  try {
+    const conditions = { likedBy: userId, tweet: tweetId };
+    const like = await Like.findOne(conditions);
+
+    if (!like) {
+      const newLike = await Like.create({
+        tweet: tweetId,
+        likedBy: userId,
+      });
+      return res
+        .status(200)
+        .json(new ApiResponse(201, newLike, "Liked successfully"));
+    } else {
+      const removeLike = await Like.findOneAndRemove(conditions);
+      return res
+        .status(200)
+        .json(new ApiResponse(201, removeLike, "Removed like successfully"));
+    }
+  } catch (error) {}
+});
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
-})
+  //TODO: get all liked videos
+  try {
+    const userId = req.user._id;
+    const likedVideos = await Like.find({ likedBy: userId });
+    if (!likedVideos) {
+      throw new ApiError(404, "No videos found");
+    }
+    res
+      .status(201)
+      .json(new ApiResponse(200, likedVideos, "Fetched successfull"));
+  } catch (error) {}
+});
 
-export {
-    toggleCommentLike,
-    toggleTweetLike,
-    toggleVideoLike,
-    getLikedVideos
-}
+export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
