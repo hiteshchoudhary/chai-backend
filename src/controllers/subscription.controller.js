6f79b7a0f0e8e6e42dc4eac8afc23602e610dd24
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-01-09 01:49:11 +0530

    feat: :sparkles: add get subscribers and channels

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { Subscription } from "../models/subscription.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { Types } from "mongoose";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
@@ -29,6 +30,7 @@ const toggleSubscription = asyncHandler(async (req, res) => {
       channel: channelId,
     });
 
+    // !Note: This aggregation can be avoided if we do not want to get a populated data on which channel got subscribed by which user.
     createdSubscription = await Subscription.aggregate([
       {
         $match: {
@@ -76,8 +78,12 @@ const toggleSubscription = asyncHandler(async (req, res) => {
       },
       {
         $addFields: {
-          subscriber: "$subscriber",
-          channel: "$channel",
+          subscriber: {
+            $first: "$subscriber",
+          },
+          channel: {
+            $first: "$channel",
+          },
         },
       },
     ]);
@@ -88,7 +94,7 @@ const toggleSubscription = asyncHandler(async (req, res) => {
       200,
       {
         subscribed: !subscription,
-        subscription: subscription ? null : createdSubscription,
+        subscription: subscription ? null : createdSubscription[0],
       },
       subscription ? "unsubscribed" : "subscribed"
     )
@@ -98,11 +104,151 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
+
+  // ! Note: Uncomment this to show one's subscribers to them only.
+  // const { _id } = req.user;
+  //
+  // if(_id.toString() !== channelId.toString()) {
+  //   throw new ApiError(403, "You are not authorized to check other's subscribers")
+  // }
+
+  const { page, size } = req.query;
+
+  const pageNumber = Number(page) ? Number.parseInt(page) : 1;
+  const querySize = Number(size) ? Number.parseInt(size) : 15;
+
+  if (querySize > 100) {
+    throw new ApiError(400, "Bro set realistic limits!!");
+  }
+
+  const subscribers = await Subscription.aggregate([
+    {
+      $match: {
+        channel: new Types.ObjectId(channelId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "subscriber",
+        foreignField: "_id",
+        as: "subscriber",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              fullName: 1,
+              username: 1,
+              avatar: 1,
+              coverImage: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        subscriber: {
+          $first: "$subscriber",
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        subscriber: 1,
+      },
+    },
+    {
+      $skip: (pageNumber - 1) * querySize,
+    },
+    {
+      $limit: querySize,
+    },
+    {
+      $replaceRoot: {
+        newRoot: "$subscriber",
+      },
+    },
+  ]);
+
+  return res.status(200).json(
+    new ApiResponse(200, {
+      subscribers,
+    })
+  );
 });
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
+
+  // ! Note: Uncomment this to show one's subscribed channels to them only.
+  // const { _id } = req.user;
+  //
+  // if(_id.toString() !== subscriberId.toString()) {
+  //   throw new ApiError(403, "You are not authorized to check other's subscribed channels")
+  // }
+
+  const { page, size } = req.query;
+
+  const pageNumber = Number(page) ? Number.parseInt(page) : 1;
+  const querySize = Number(size) ? Number.parseInt(size) : 15;
+
+  if (querySize > 100) {
+    throw new ApiError(400, "Bro set realistic limits!!");
+  }
+
+  const subscribedChannels = await Subscription.aggregate([
+    {
+      $match: {
+        subscriber: new Types.ObjectId(subscriberId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "channel",
+        foreignField: "_id",
+        as: "channel",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              fullName: 1,
+              avatar: 1,
+              username: 1,
+              coverImage: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        channel: {
+          $first: "$channel",
+        },
+      },
+    },
+    {
+      $skip: (pageNumber - 1) * querySize,
+    },
+    {
+      $limit: querySize,
+    },
+    {
+      $replaceRoot: {
+        newRoot: "$channel",
+      },
+    },
+  ]);
+
+  return res.status(200).json(
+    new ApiResponse(200, {
+      channels: subscribedChannels,
+    })
+  );
 });
 
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,19 +1,19 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
-    getSubscribedChannels,
-    getUserChannelSubscribers,
-    toggleSubscription,
-} from "../controllers/subscription.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+  getSubscribedChannels,
+  getUserChannelSubscribers,
+  toggleSubscription,
+} from "../controllers/subscription.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router
-    .route("/c/:channelId")
-    .get(getSubscribedChannels)
-    .post(toggleSubscription);
+  .route("/c/:channelId")
+  .get(getUserChannelSubscribers)
+  .post(toggleSubscription);
 
-router.route("/u/:subscriberId").get(getUserChannelSubscribers);
+router.route("/u/:subscriberId").get(getSubscribedChannels);
 
-export default router
+export default router;
