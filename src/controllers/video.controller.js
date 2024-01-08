0a7b5a38c9d70d2232fd4ec215455427ab1d8db8
souvik <ghoshsouvik167@gmail.com>
2024-01-08 22:35:03 +0530

    getallvideos controller created

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -9,12 +9,53 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
-    //TODO: get all videos based on query, sort, pagination
+
+    let filter={}
+    let option={
+        limit,
+        skip:(page-1)*limit
+    }
+    if (query){
+        filter.$or=[
+        {
+            title:{$regex:/query/i}
+        }
+        {
+            description:{regex:/query/i}
+        }
+        ]
+    }
+    if(userId){
+        filter.owner=userId
+    }
+
+    if(Object.Keys(filter).length===0){
+        throw ApiError(400,"query must be requried")
+    }
+
+    const allVideos= await Video.find(filter,null,option)?.sort({sortBy:sortType})?.toArray()
+    if(!allVideos){
+        throw ApiError(401,"videos not found")
+    }
+
+   return res.status(201).json(
+    ApiResponse(200,allVideos,"videos found sucessfully")
+
+    )
+
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+    if(!title || !description){
+        throw ApiError(409,"title and description must be requried")
+    }
+
+    
+
+
+
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
