e2632b205b7f2fe4bd60cebdcca7f163ef2eb46a
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-01-10 23:03:57 +0530

    delete old image when avatar is update

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,11 +1,11 @@
-import {v2 as cloudinary} from "cloudinary"
-import fs from "fs"
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
 
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -26,6 +26,16 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 }
 
+const deleteOnCloudinary = async (url) => {
+    try {
+        await cloudinary.api.delete_resources([url], { type: 'upload', resource_type: 'image' }).then(console.log);
+
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
 
 
-export {uploadOnCloudinary}
+
+export { uploadOnCloudinary, deleteOnCloudinary };
