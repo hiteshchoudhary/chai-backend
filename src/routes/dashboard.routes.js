ea5c7a061d6f59fd802c105b5f413eb5d7226345
silentkiller6092 <shivampandey6092@gmail.com>
2024-01-16 22:13:55 +0530

    Completed the Dashboard controller added Controller

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,15 +1,15 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
-    getChannelStats,
-    getChannelVideos,
-} from "../controllers/dashboard.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+  getChannelStats,
+  getChannelVideos,
+} from "../controllers/dashboard.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/stats").get(getChannelStats);
-router.route("/videos").get(getChannelVideos);
+router.route("/stats/:userId").get(getChannelStats);
+router.route("/videos/:userId").get(getChannelVideos);
 
-export default router
+export default router;
