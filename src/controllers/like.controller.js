7a25550937d4db2f8501b61584f91c47c323e63e
Dipesh Pal <codewithdipesh2022@gmail.com>
2024-01-24 22:41:25 +0530

    added like controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -119,16 +119,92 @@ const toggleTweetLike = asyncHandler(async (req, res) => {
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
     const userId = req.user?._id;
-    const likedVideos = await Like.aggregate([
-        {
-            $match:{
-                likedBy:new mongoose.Types.ObjectId(userId),
-                video:{$exist}
+    try {
+        const likedVideos = await Like.aggregate([
+            {
+                $match: {
+                    likedBy: new mongoose.Types.ObjectId(userId),
+                }
+            },
+            {
+                $lookup: {
+                    from: "videos",
+                    localField: "video",
+                    foreignField: "_id",
+                    as: "likedVideos"
+                }
+            },
+            {
+                $unwind: "$likedVideos"
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    let: { owner_id: "$likedVideos.owner" },
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: { $eq: ["$_id", "$$owner_id"] }
+                            }
+                        },
+                        {
+                            $project: {
+                                _id: 0,
+                                username: 1,
+                                avatar: 1,
+                                fullName: 1
+                            }
+                        }
+                    ],
+                    as: "owner"
+                }
+            },
+            {
+                $unwind: { path: "$owner", preserveNullAndEmptyArrays: true }
+            },
+            {
+                $project: {
+                    _id: "$likedVideos._id",
+                    title: "$likedVideos.title",
+                    thumbnail: "$likedVideos.thumbnail",
+                    owner: {
+                        username: "$owner.username",
+                        avatar: "$owner.avatar",
+                        fullName: "$owner.fullName"
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    likedVideos: { $push: "$$ROOT" }
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    likedVideos: 1
+                }
             }
-        },{
-
+        ]);
+        
+        if (likedVideos.length === 0) {
+            return res
+                .status(404)
+                .json(new ApiResponse(404, [], "No liked videos found"));
         }
-    ])
+        
+        return res
+        .status(200)
+        .json(new ApiResponse(200,likedVideos,"LikedVideos fetched Successfully!"))
+    
+    
+    } catch (e) {
+        throw new ApiError(500,e?.message || "Unable to fecth LikedVideos!")
+        
+    }
+
+
 })
 
 export {
