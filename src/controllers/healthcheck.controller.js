4c30bb099896ad7120b7e4fdbc5ba4e3e53b9db8
Dev Prajapati <152001915+Devprajapapati@users.noreply.github.com>
2024-03-07 07:14:33 +0530

    Update healthcheck.controller.js

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -3,11 +3,16 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 
-const healthcheck = asyncHandler(async (req, res) => {
+
+const healthcheck = asyncHandler(async (req, res) => { 
     //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    return res
+    .status(200)
+    .json(new apiResponse(200,{},"Server is healthy!!"));
 })
 
 export {
     healthcheck
     }
+    
     
