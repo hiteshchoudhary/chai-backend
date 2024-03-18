7d4ed10bd823b1532087ca319db2c4106309c9aa
pradeep <pb63827@gmail.com>
2024-03-18 13:10:50 +0530

    added video controllers

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -15,7 +15,7 @@ router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router
     .route("/")
-    .get(getAllVideos)
+    .get( getAllVideos)
     .post(
         upload.fields([
             {
